e420401ff26934061e38fb160429814683b8aec0
Bekbolsun Alkozhoev <alkozhoev.b@gmail.com>
2022-12-21 23:11:43 +0500

    work_4(1)

--- work_4/Program.cs
+++ work_4/Program.cs
@@ -1,6 +1,9 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine()?? "");
+if (N < 0 ){
+    Console.WriteLine("Напишите число больше 0");
+}
 int result = 0;
 while(1 < N) {
     result = N % 2;
