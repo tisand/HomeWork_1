6124596e12ee0fa19b68f07db5a5ebdf7cf4d5ac
Bekbolsun Alkozhoev <alkozhoev.b@gmail.com>
2022-12-21 23:06:43 +0500

    work_4

---
+++ work_4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+int result = 0;
+while(1 < N) {
+    result = N % 2;
+    if (result == 0){
+        Console.WriteLine($"Количество четных чисел в диапозоне {N}");
+    }
+    N--;
+}
